eb1c781132fde6bce997e91f3f6118c01588532f
Arjun Kathuria <arjun.kathuria8@gmail.com>
2024-07-10 20:26:20 +0530

    [GHC-Upgrade]: Fix breakages with GHC 9.2.8 when building `Shims.hs`
    This commit updates the code to be able to build with GHC 9.2.8.
    This project hasn't been updated in a while so had quite a few breakages.
    
    This commits fix the breakages using the new "ghc"-as-a-library syntax.
    It uses the conditional compile directives to use the updated syntax with
    GHC 9.2.8 while maintaining the older syntax with previous versions.
    
    "ghc" as in the library had a lot of internal changes as well as external API
    and module export changes between version 8.10 and 9.2.
    These changes reflect that fact.
    The "ghc-plugins" part also had a few changes to take into account.
    
    The Lot of __GLASGOW_HASKELL__ <= 902 checks ensure that only
    compatible syntax gets compiled with GHC 9.2.x
    
    Main breakages that i can remember were:
    - "Located" type fixes
    - "SrcSpanAnn" type fixes
    - ghc the library moved a lot of previous types to using type-families
    - makes things compatible with newer syntax and version.
    - conditional compiles compatibility and making sure right things compiled.
    - more of these that i'm sure i can't remember at the moment.

--- src/Data/Record/Plugin/Shim.hs
+++ src/Data/Record/Plugin/Shim.hs
@@ -38,6 +38,7 @@ module Data.Record.Plugin.Shim (
   , kindedTyVar
   , hsTyVarLName
   , setDefaultSpecificity
+  , mkSrcSpanAnn
 
     -- * Re-exports
 
@@ -58,9 +59,15 @@ module Data.Record.Plugin.Shim (
   , module GHC.Hs
   , module GHC.Plugins
   , module GHC.Tc.Types.Evidence
-  , module GHC.Types.Basic
   , module GHC.Types.Name.Cache
   , module GHC.Utils.Error
+#endif
+#if __GLASGOW_HASKELL__ >= 902
+  , module GHC.Types.SourceText
+  , module GHC.Driver.Errors
+  , module GHC.Utils.Logger
+#else
+  , module GHC.Types.Basic
 #endif
   ) where
 
@@ -91,17 +98,26 @@ import GHC.Core.ConLike (ConLike)
 import GHC.Core.PatSyn (PatSyn)
 import GHC.Data.Bag (listToBag, emptyBag)
 import GHC.Driver.Main (getHscEnv)
-import GHC.Hs hiding (LHsTyVarBndr, HsTyVarBndr, HsModule, mkFunBind)
+import GHC.Hs hiding (LHsTyVarBndr, HsTyVarBndr, HsModule, mkFunBind, AnnType, AnnRec, AnnLet, AnnLam, AnnCase)
 import GHC.Parser.Annotation (IsUnicodeSyntax(NormalSyntax))
 import GHC.Plugins hiding ((<>), getHscEnv, putLogMsg)
 import GHC.Tc.Types.Evidence (HsWrapper(WpHole))
-import GHC.Types.Basic (SourceText(NoSourceText))
 import GHC.Types.Name.Cache (NameCache(nsUniqs))
-import GHC.Utils.Error (Severity(SevError, SevWarning), mkErrMsg, mkWarnMsg)
 
 import qualified GHC.Hs      as GHC
 import qualified GHC.Plugins as GHC
 
+#endif
+
+#if __GLASGOW_HASKELL__ >= 902
+import GHC.Types.SourceText (SourceText(NoSourceText))
+import GHC.Utils.Logger (getLogger)
+import qualified GHC.Utils.Logger as GHC (putLogMsg, Logger, LogAction)
+import GHC.Utils.Error (Severity(SevError, SevWarning), mkErr, mkWarnMsg)
+import GHC.Driver.Errors (printOrThrowWarnings)
+#elif __GLASGOW_HASKELL__ >= 900 && __GLASGOW_HASKELL__ < 902
+import GHC.Types.Basic (SourceText(NoSourceText))
+import GHC.Utils.Error (Severity(SevError, SevWarning), mkErrMsg, mkWarnMsg)
 #endif
 
 {-------------------------------------------------------------------------------
@@ -110,10 +126,18 @@ import qualified GHC.Plugins as GHC
 
 -- | Optionally @qualified@ import declaration
 importDecl :: ModuleName -> Bool -> LImportDecl GhcPs
-importDecl name qualified = noLoc $ ImportDecl {
-      ideclExt       = defExt
+importDecl name qualified = noLocA $ ImportDecl {
+#if __GLASGOW_HASKELL__ < 902
+    ideclExt       = defExt
+#else
+    ideclExt       = noAnn
+#endif
     , ideclSourceSrc = NoSourceText
+#if __GLASGOW_HASKELL__ < 902
     , ideclName      = noLoc name
+#else
+    , ideclName      = noLocA name
+#endif
     , ideclPkgQual   = Nothing
     , ideclSafe      = False
     , ideclImplicit  = False
@@ -131,14 +155,27 @@ importDecl name qualified = noLoc $ ImportDecl {
 #endif
     }
 
+#if __GLASGOW_HASKELL__ < 902
 conPat :: Located RdrName -> HsConPatDetails GhcPs -> Pat GhcPs
+#else
+conPat :: XRec GhcPs (ConLikeP GhcPs) -> HsConPatDetails GhcPs -> Pat GhcPs
+#endif
 #if __GLASGOW_HASKELL__ < 900
 conPat x y = ConPatIn x y
-#else
+#elif __GLASGOW_HASKELL__ >= 900 && __GLASGOW_HASKELL__ < 902
 conPat x y = ConPat noExtField x y
+#else
+conPat x y = ConPat EpAnnNotUsed x y
 #endif
 
+#if __GLASGOW_HASKELL__ < 902
 mkFunBind :: Located RdrName -> [LMatch GhcPs (LHsExpr GhcPs)] -> HsBind GhcPs
+#else
+mkFunBind :: LocatedN RdrName
+          -> [GenLocated
+               SrcSpanAnnA (Match GhcPs (GenLocated SrcSpanAnnA (HsExpr GhcPs)))]
+          -> HsBind GhcPs
+#endif
 #if __GLASGOW_HASKELL__ < 810
 mkFunBind = GHC.mkFunBind
 #else
@@ -154,10 +191,16 @@ type HsModule = GHC.HsModule
 type LHsModule = Located HsModule
 type LRdrName  = Located RdrName
 
+#if __GLASGOW_HASKELL__ < 902
 putLogMsg :: DynFlags -> WarnReason -> Severity -> SrcSpan -> SDoc -> IO ()
+#else
+putLogMsg :: GHC.Logger -> GHC.LogAction
+#endif
 #if __GLASGOW_HASKELL__ < 900
 putLogMsg flags reason sev srcspan =
     GHC.putLogMsg flags reason sev srcspan (defaultErrStyle flags)
+#elif __GLASGOW_HASKELL__ < 902
+putLogMsg = GHC.putLogMsg
 #else
 putLogMsg = GHC.putLogMsg
 #endif
@@ -198,21 +241,36 @@ hsFunTy = HsFunTy
 hsFunTy ext = HsFunTy ext (HsUnrestrictedArrow NormalSyntax)
 #endif
 
+#if __GLASGOW_HASKELL__ < 902
 userTyVar ::
      XUserTyVar pass
   -> Located (IdP pass)
   -> HsTyVarBndr pass
+#else
+userTyVar ::
+  XUserTyVar pass
+  -> XRec pass (IdP pass)
+  -> GHC.HsTyVarBndr () pass
+#endif
 #if __GLASGOW_HASKELL__ < 900
 userTyVar = UserTyVar
 #else
 userTyVar ext = UserTyVar ext ()
 #endif
 
+#if __GLASGOW_HASKELL__ < 902
 kindedTyVar ::
      XKindedTyVar pass
   -> Located (IdP pass)
   -> LHsKind pass
   -> HsTyVarBndr pass
+#else
+kindedTyVar ::
+  XKindedTyVar pass
+  -> LIdP pass
+  -> LHsKind pass
+  -> GHC.HsTyVarBndr () pass
+#endif
 #if __GLASGOW_HASKELL__ < 900
 kindedTyVar = KindedTyVar
 #else
@@ -220,7 +278,11 @@ kindedTyVar ext = KindedTyVar ext ()
 #endif
 
 -- | Like 'hsTyVarName', but don't throw away the location information
+#if __GLASGOW_HASKELL__ < 902
 hsTyVarLName :: HsTyVarBndr GhcPs -> LRdrName
+#else
+hsTyVarLName :: GHC.HsTyVarBndr flag GhcPs -> LIdP GhcPs
+#endif
 #if __GLASGOW_HASKELL__ < 900
 hsTyVarLName (UserTyVar   _ n  ) = n
 hsTyVarLName (KindedTyVar _ n _) = n
@@ -233,17 +295,30 @@ hsTyVarLName (KindedTyVar _ _ n _) = n
 #if __GLASGOW_HASKELL__ < 900
 setDefaultSpecificity :: LHsTyVarBndr pass -> GHC.LHsTyVarBndr pass
 setDefaultSpecificity = id
-#else
+#elif __GLASGOW_HASKELL__  >= 900 && __GLASGOW_HASKELL__ < 902
 setDefaultSpecificity :: LHsTyVarBndr pass -> GHC.LHsTyVarBndr Specificity pass
+setDefaultSpecificity (L l v) = L l $ case v of
+    UserTyVar   ext () name      -> UserTyVar   ext SpecifiedSpec name
+    KindedTyVar ext () name kind -> KindedTyVar ext SpecifiedSpec name kind
+    XTyVarBndr  ext              -> XTyVarBndr  ext
+#elif __GLASGOW_HASKELL__  >= 902
+setDefaultSpecificity :: GenLocated l (GHC.HsTyVarBndr () pass)
+                      -> GenLocated l (GHC.HsTyVarBndr Specificity pass)
 setDefaultSpecificity (L l v) = L l $ case v of
     UserTyVar   ext () name      -> UserTyVar   ext SpecifiedSpec name
     KindedTyVar ext () name kind -> KindedTyVar ext SpecifiedSpec name kind
     XTyVarBndr  ext              -> XTyVarBndr  ext
 #endif
 
+-- New helper function
+mkSrcSpanAnn :: SrcSpan -> SrcSpanAnnA
+mkSrcSpanAnn l = SrcSpanAnn EpAnnNotUsed l
+
 patLoc :: SrcSpan -> Pat (GhcPass id) -> LPat (GhcPass id)
 #if __GLASGOW_HASKELL__ >= 810 && __GLASGOW_HASKELL__ <= 920
-patLoc l p = L l p
+patLoc l p = L l' p
+  where
+    l' = mkSrcSpanAnn l
 #else
 patLoc _ p = p
 #endif
@@ -255,7 +330,7 @@ viewConPat (ConPatIn a b) = Just (a, b)
 viewConPat :: LPat (GhcPass id) -> Maybe (Located (IdP (GhcPass id)), HsConPatDetails (GhcPass id))
 viewConPat (L _ (ConPatIn a b)) = Just (a, b)
 #elif __GLASGOW_HASKELL__ >= 900
-viewConPat :: LPat (GhcPass id) -> Maybe (Located (ConLikeP (GhcPass id)), HsConPatDetails (GhcPass id))
+viewConPat :: LPat (GhcPass id) -> Maybe (XRec (GhcPass id) (ConLikeP (GhcPass id)), HsConPatDetails (GhcPass id))
 viewConPat (L _ (ConPat _ext a b)) = Just (a, b)
 #endif
 viewConPat _ = Nothing
